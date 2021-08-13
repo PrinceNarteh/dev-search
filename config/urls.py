f39c69c56b11e399299921a770d9bbb50296a921
Prince Narteh <prinarteh@gmail.com>
2021-08-13 12:14:54 +0000

    Add User Profile Template

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('projects.urls')),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
