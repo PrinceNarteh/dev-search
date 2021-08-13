b90b6ca3f5f0b829e3eb0fd575984de853d341ea
Prince Narteh <prinarteh@gmail.com>
2021-08-13 12:41:10 +0000

    Add Profile Model

--- config/urls.py
+++ config/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('projects.urls')),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('projects/', include('projects.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
+    short_intro = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_image = models.ImageField(
+        null=True, blank=True, upload_to='profiles/', default='profiles/user-default.png')
+    social_github = models.CharField(max_length=200, blank=True, null=True)
+    social_twitter = models.CharField(max_length=200, blank=True, null=True)
+    social_linkedin = models.CharField(max_length=200, blank=True, null=True)
+    social_youtube = models.CharField(max_length=200, blank=True, null=True)
+    social_website = models.CharField(max_length=200, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.user.username)
