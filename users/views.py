aee6e7d7d78a485d546948d15c51533703d7f652
Prince Narteh <prinarteh@gmail.com>
2021-08-14 22:57:45 +0000

    Add User Profile Detail Page

--- users/views.py
+++ users/views.py
@@ -5,4 +5,13 @@ from .models import Profile
 def profiles(request):
     profiles = Profile.objects.all()
     print(profiles)
-    return render(request, 'users/profiles.html', { "profiles" : profiles})
+    return render(request, 'users/profiles.html', {"profiles": profiles})
+
+
+def userProfile(request, pk: str):
+    profile = Profile.objects.get(id=pk)
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description="")
+    context = {"profile": profile, "top_skills": top_skills,
+               "other_skills": other_skills}
+    return render(request, 'users/user-profile.html', context)
