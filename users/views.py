e65029893280f966d375b1d9c5c9136ee0721c9f
Prince Narteh <prinarteh@gmail.com>
2021-08-15 06:10:10 +0000

    Add Login Handler

--- users/views.py
+++ users/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from .models import Profile
+
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username not found.')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            print('Username or password is incorrect.')
+    return render(request, 'user/login_register.html')
 
 
 def profiles(request):
