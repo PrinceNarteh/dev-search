77da08b1b7c6a84f6e025299344923289621517c
Prince Narteh <prinarteh@gmail.com>
2021-08-15 06:54:01 +0000

    Add Register Handler

--- users/views.py
+++ users/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 
 
 def loginUser(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -16,18 +18,38 @@ def loginUser(request):
         try:
             user = User.objects.get(username=username)
         except:
-            messages.error(request,'Username not found.')
+            messages.error(request, 'Username not found.')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('profiles')
         else:
             messages.error(request, 'Username or password is incorrect.')
-    return render(request, 'user/login_register.html')
+    return render(request, 'users/login_register.html')
+
 
 def logoutUser(request):
     logout(request)
     return redirect('login')
+
+
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            messages.success(request, "User account was created!")
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "Error occurred durring registration!")
+    context = {"page": page, "form": form}
+    return render(request, 'users/login_register.html', context)
 
 
 def profiles(request):
